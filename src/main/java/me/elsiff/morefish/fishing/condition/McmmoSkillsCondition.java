9e283283138adf7b6c19c94ecc93f717be0cead2
Musician101 <bruceabivens@gmail.com>
2024-04-27 22:12:58 -0700

    Static import.

--- src/main/java/me/elsiff/morefish/fishing/condition/McmmoSkillsCondition.java
+++ src/main/java/me/elsiff/morefish/fishing/condition/McmmoSkillsCondition.java
@@ -2,7 +2,6 @@ package me.elsiff.morefish.fishing.condition;
 
 import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
 import me.elsiff.morefish.hooker.McmmoHooker;
-import me.elsiff.morefish.hooker.PluginHooker;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -10,12 +9,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 
 import static me.elsiff.morefish.MoreFish.getPlugin;
+import static me.elsiff.morefish.hooker.PluginHooker.checkEnabled;
 
 public record McmmoSkillsCondition(@NotNull Map<PrimarySkillType, Integer> skills) implements FishCondition {
 
     public boolean check(@NotNull Item caught, @NotNull Player fisher) {
         McmmoHooker mcmmoHooker = getPlugin().getMcmmo();
-        PluginHooker.checkEnabled(mcmmoHooker, fisher.getServer().getPluginManager());
+        checkEnabled(mcmmoHooker, fisher.getServer().getPluginManager());
         return skills.entrySet().stream().allMatch(e -> mcmmoHooker.skillLevelOf(fisher, e.getKey()) >= e.getValue());
     }
 }
