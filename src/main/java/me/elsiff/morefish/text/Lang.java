795bc489804360effadded9b36312e6def8ba572
Musician101 <bruceabivens@gmail.com>
2024-06-01 16:43:54 -0700

    Fish name tag resolver should return display name, not internal name.

--- src/main/java/me/elsiff/morefish/text/Lang.java
+++ src/main/java/me/elsiff/morefish/text/Lang.java
@@ -158,7 +158,7 @@ public class Lang {
 
     @NotNull
     public static TagResolver fishName(@NotNull Fish fish) {
-        return Lang.tagResolver("fish-name", fish.name());
+        return Lang.tagResolver("fish-name", fish.type().displayName());
     }
 
     @NotNull
